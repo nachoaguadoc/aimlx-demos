dc015b9fb671bbfb4bd0fab38abd068f4a4424ec
Ignacio Aguado <nachoaguadoc@gmail.com>
2017-04-13 18:25:07 +0200

    Suggestions for dual encoder ubuntu

--- static/javascripts/chatbot_ubuntu.js
+++ static/javascripts/chatbot_ubuntu.js
@@ -9,14 +9,15 @@ function new_question(question) {
 
 function new_chatbot_answer(candidates_nn, candidates_solr) {
 	$('.robot').show();
-	for (c in candidates_nn) {
+	suggestions_random = get_random_suggestions(suggestions);
+    load_suggestions(suggestions_random);	
+   	for (c in candidates_nn) {
 		$('#answer_nn').html($('#answer_nn').html() + '<div class="col-md-12 answer_box"><div class="panel panel-default neural"><div class="panel-body">' + candidates_nn[c] + ' </div></div></div>');
 	}
 	for (c in candidates_solr) {
 		$('#answer_solr').html($('#answer_solr').html() + '<div class="col-md-12 answer_box"><div class="panel panel-default benchmark"><div class="panel-body">' + candidates_solr[c] + ' </div></div></div>');
 	}
-	suggestions_random = get_random_suggestions(suggestions);
-    load_suggestions(suggestions_random);	
+    
 	$('.col-md-6').matchHeight();
 	stop_spinner();
 }
