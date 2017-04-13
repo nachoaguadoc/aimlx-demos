5a5e4ed178c967a88e149f445789f4d0053bb3cf
Ignacio Aguado <nachoaguadoc@gmail.com>
2017-04-13 18:12:40 +0200

    redundancies

--- static/javascripts/common.js
+++ static/javascripts/common.js
@@ -5,18 +5,3 @@ function start_spinner() {
 function stop_spinner() {
 	$('#spinner').remove();
 }
-
-$(document).ready(function(){
-	$('#input_text').keyup(function(e){
-	    if(e.keyCode == 13) {
-	    	input_text = $('#input_text').val();
-	    	if (input_text != '') submit(input_text, $('#project_value').text().toLowerCase());
-	    }
-	});
-
-	$('#search_button').click(function(e){
-		input_text = $('#input_text').val();
-	    if (input_text != '') submit(input_text, $('#project_value').text().toLowerCase());
-	})
-	
-});
