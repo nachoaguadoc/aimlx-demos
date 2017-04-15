e298413a22929d89925666cba200746d9d947c98
nachoaguadoc <nachoaguadoc@gmail.com>
2017-04-15 16:07:04 +0000

    kpextraction javascript fix

--- static/javascripts/kp_extraction.js
+++ static/javascripts/kp_extraction.js
@@ -1,6 +1,6 @@
 function submit(input_text) {
 	$("#last_group").append('<i class="fa fa-spinner fa-spin" id="spinner"></i>');
-	console.log("url input: ", input_text)
+	console.log("url input: ", input_text);
 	$('#input_text').val('');
 	url = "/kp";
 	$.ajax({
@@ -15,3 +15,18 @@ function submit(input_text) {
 	  }
 	});
 }
+
+$(document).ready(function(){
+
+    $('#input_text').keyup(function(e){
+        if(e.keyCode == 13) {
+            input_text = $('#input_text').val();
+            if (input_text != '') submit(input_text, $('#project_value').text());
+        }
+    });
+
+    $('#search_button').click(function(e){
+        input_text = $('#input_text').val();
+        if (input_text != '') submit(input_text, $('#project_value').text());
+    })
+});
