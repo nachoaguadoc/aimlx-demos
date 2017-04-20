0207e01a9d305172d6a23d432903d76357c79523
Ignacio Aguado <nachoaguadoc@gmail.com>
2017-04-20 11:58:54 +0200

    Fixed glitch on swisscom javascript

--- static/javascripts/chatbot_swisscom.js
+++ static/javascripts/chatbot_swisscom.js
@@ -39,5 +39,17 @@ function submit(input_text) {
 }
 
 $(document).ready(function(){
+
+	$('#input_text').keyup(function(e){
+	    if(e.keyCode == 13) {
+	    	input_text = $('#input_text').val();
+	    	if (input_text != '') submit(input_text, $('#project_value').text().toLowerCase());
+	    }
+	});
+
+	$('#search_button').click(function(e){
+		input_text = $('#input_text').val();
+	    if (input_text != '') submit(input_text, $('#project_value').text().toLowerCase());
+	})
 	$('.robot').hide();
 });
