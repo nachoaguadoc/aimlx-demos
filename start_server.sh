30f39cec763d08c14e6412d9603525abb26dcb3e
nachoaguadoc <nachoaguadoc@gmail.com>
2017-05-12 14:50:56 +0000

    Removed script

--- start_server.sh
+++
@@ -1,5 +0,0 @@
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LANGUAGE=en_US.UTF-8
-
-uwsgi --close-on-exec2 --http 0.0.0.0:4343 --wsgi-file server.py --callable app --master -b 32768 --close-on-exec
