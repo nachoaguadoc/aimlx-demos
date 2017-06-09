f718eaa0de0f1732e95b1567e300c1383aa022d8
Ignacio Aguado <nachoaguadoc@gmail.com>
2017-06-09 19:51:35 +0200

    Suggestions for neural programmer

--- static/javascripts/neural_programmer.js
+++ static/javascripts/neural_programmer.js
@@ -7,8 +7,8 @@ function new_question(question) {
 
 function new_neural_programmer_answer(np) {
 	$('#answer_np').text(np);
-	//suggestions_random = get_random_suggestions(suggestions);
-    //load_suggestions(suggestions_random);
+	suggestions_random = get_random_suggestions(suggestions);
+    load_suggestions(suggestions_random);
 	stop_spinner();
 }
 
@@ -74,15 +74,15 @@ $(document).ready(function(){
 	    if (input_text != '') submit(input_text, $('#project_value').text().toLowerCase());
 	})
 
-	//$.getJSON("../static/javascripts/lists/chatbot_seq2seq_ubuntu.json", function(json) {
-	//	suggestions = json.candidates;
-	//	suggestions_random = get_random_suggestions(suggestions);
-	//   load_suggestions(suggestions_random);
-	//});
+	$.getJSON("../static/javascripts/lists/neural_programmer.json", function(json) {
+		suggestions = json.questions;
+		suggestions_random = get_random_suggestions(suggestions);
+	   load_suggestions(suggestions_random);
+	});
 
 	
 	//table = '<div id="table-wrap" style="height: 664px;"><div id="table"><table class="clean table"><thead><tr><th>S.No.</th><th>Name of Kingdom</th><th>Name of King</th><th>No. of villages</th><th>Capital</th><th>Names of districts</th></tr></thead><tbody><tr><td>1.</td><td>Sihag</td><td>Chokha Singh</td><td>150</td><td>Suin</td><td>Rawatsar, Baramsar, Purabsar Dandusar, Gandaisi</td></tr><tr><td>2.</td><td>Beniwal</td><td>Raisal Singh</td><td>150</td><td>Rasalana</td><td>Bhukarka, Sanduri, Manoharpur, Kooi, Bae</td></tr><tr><td>3.</td><td>Johiya</td><td>Sher Singh</td><td>600</td><td>Bhurupal</td><td>Jaitpur, Kumanu, Mahajan, Peepasar, Udasar</td></tr><tr><td>4.</td><td>Punia</td><td>Kanha Singh</td><td>300</td><td>Luddi</td><td>Bhadra, Ajitpura, Sidhmukh, Rajgarh, Dadrewa, Sankhoo</td></tr><tr><td>5.</td><td>Saharan</td><td>Pula Singh</td><td>300</td><td>Bhadang</td><td>Khejra, Phoglo, Buchawas, Sui, Badnu, Sirsila</td></tr><tr><td>6.</td><td>Godara</td><td>Pandu Singh</td><td>700</td><td>Shekhsar</td><td>Shekhsar, Pundrasar, Gusainsar (Bada), Gharsisar, Garibdesar, Rungaysar, Kalu</td></tr><tr><td>7.</td><td>Kaswan</td><td>Kanwarpal Singh</td><td>400</td><td>Sidhmukh</td><td></td></tr></tbody></table></div>    </div>'
-	table = '<div id="table-wrap" style="height: 664px;"><div id="table"><table class="clean table"><thead><tr><th>Package</th><th>Broadband speed (Mbps)</th><th>International time (Minutes)</th><th>Roaming (Days)</th><th>Price (CHF)</th></tr></thead><tbody><tr><td>InOne XL</td><td>200</td><td>1000</td><td>365</td><td>180</td></tr><tr><td>InOne L</td><td>200</td><td>300</td><td>365</td><td>120</td></tr><tr><td>InOne M</td><td>200</td><td>100</td><td>60</td><td>90</td></tr><tr><td>InOne S</td><td>20</td><td>60</td><td>45</td><td>70</td></tr><tr><td>InOne XS</td><td>2</td><td>30</td><td>30</td><td>60</td><tr></tbody></table></div></div>'
+	table = '<div id="table-wrap" style="height: 664px;"><div id="table"><table class="clean table"><thead><tr><th>Package</th><th>Broadband speed (Mbps)</th><th>International time (Minutes)</th><th>Roaming (Days)</th><th>Price (CHF)</th><th>Description</th></tr></thead><tbody><tr><td>InOne XL</td><td>200</td><td>1000</td><td>365</td><td>180</td><td>With highspeed internet, ideal for business</td></tr><tr><td>InOne L</td><td>200</td><td>300</td><td>365</td><td>120</td><td>Complete package thought for travellers.</td></tr><tr><td>InOne M</td><td>200</td><td>100</td><td>60</td><td>90</td><td>Top seller package, ideal for teenagers to use the social media.</td></tr><tr><td>InOne S</td><td>20</td><td>60</td><td>45</td><td>70</td><td>Fast internet with an affordable price. Convenient for holidays abroad.</td></tr><tr><td>InOne XS</td><td>2</td><td>30</td><td>30</td><td>50</td><td>Package cheap, simple and easy.</td><tr></tbody></table></div></div>'
 
 	$('#table_container').html(table)
 });
