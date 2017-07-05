3c7958ebcb19a69f05386eabfc1e7f94bd9d0db9
Ignacio Aguado <nachoaguadoc@gmail.com>
2017-07-05 12:38:26 +0200

    Hidden recommendations

--- static/javascripts/neural_programmer.js
+++ static/javascripts/neural_programmer.js
@@ -40,8 +40,8 @@ function new_neural_programmer_answer(np, debug) {
 
 	$('#debug').html(steps);
 	$("#debug").css('visibility', 'visible');
-	suggestions_random = get_random_suggestions(suggestions);
-    load_suggestions(suggestions_random);
+	//suggestions_random = get_random_suggestions(suggestions);
+    //load_suggestions(suggestions_random);
 	stop_spinner();
 }
 
@@ -117,11 +117,11 @@ $(document).ready(function(){
 	    if (input_text != '') submit(input_text, $('#project_value').text().toLowerCase());
 	})
 
-	$.getJSON("../static/javascripts/lists/neural_programmer_swisscom.json", function(json) {
-		suggestions = json.questions;
-		suggestions_random = get_random_suggestions(suggestions);
-	  load_suggestions(suggestions_random);
-	});
+	//$.getJSON("../static/javascripts/lists/neural_programmer_swisscom.json", function(json) {
+	//	suggestions = json.questions;
+	//	suggestions_random = get_random_suggestions(suggestions);
+	//  load_suggestions(suggestions_random);
+	//});
 	
 	$(document).on('mouseenter', '.step', function() {
     	var col = $(this).find('.col')[0]
