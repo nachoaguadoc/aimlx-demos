c2b6d0868a6f8f5d408ffbf1b64f93e115222bc1
Ignacio Aguado <nachoaguadoc@gmail.com>
2017-07-12 14:20:20 +0200

    Added text of tutorial verification

--- static/javascripts/neural_programmer_landing.js
+++ static/javascripts/neural_programmer_landing.js
@@ -21,14 +21,17 @@ function getCookie(cname) {
 
 $(document).ready(function(){
 
-	$('.thumbnail').matchHeight();
-	$('.button-go').addClass("corner");
-
 	if (isNewUser('user_id')) {
 		console.log("New user. Needs to complete tutorial.")
 		$('#demo_simple').addClass("disabled");
 		$('#demo_steps').addClass("disabled");
+		$("#demo_simple_desc").html($("#demo_simple_desc").html() + "<b> You must complete the tutorial first to activate this demo.</b>");
+		$("#demo_steps_desc").html($("#demo_steps_desc").html() + "<b> You must complete the tutorial first to activate this demo.</b>")
+
 	} else {
 		console.log("Already completed tutorial")
 	}
+
+	$('.thumbnail').matchHeight();
+	$('.button-go').addClass("corner");
 });
