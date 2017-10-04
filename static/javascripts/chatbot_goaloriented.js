f37c9c3250e549447f232de29e79abd01a02d8d7
IlievskiV <ilievski.vladimir@live.com>
2017-10-04 14:18:24 +0200

    Add javascript

---
+++ static/javascripts/chatbot_goaloriented.js
@@ -0,0 +1,102 @@
+var me = {};
+me.avatar = "https://lh6.googleusercontent.com/-lr2nyjhhjXw/AAAAAAAAAAI/AAAAAAAARmE/MdtfUmC0M4s/photo.jpg?sz=48";
+
+var you = {};
+you.avatar = "https://a11.t26.net/taringa/avatares/9/1/2/F/7/8/Demon_King1/48x48_5C5.jpg";
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+//-- No use time. It is a javaScript effect.
+function insertChat(who, text, time = 0){
+    var control = "";
+    var date = formatAMPM(new Date());
+
+    if (who == 'me'){
+
+        control = '<li style="width:100%">' +
+                        '<div class="msj macro">' +
+                        '<div class="avatar"><img class="img-circle" style="width:100%;" src="'+ me.avatar +'" /></div>' +
+                            '<div class="text text-l">' +
+                                '<p>'+ text +'</p>' +
+                                '<p><small>'+date+'</small></p>' +
+                            '</div>' +
+                        '</div>' +
+                    '</li>';
+    }else{
+        control = '<li style="width:100%;">' +
+                        '<div class="msj-rta macro">' +
+                            '<div class="text text-r">' +
+                                '<p>'+text+'</p>' +
+                                '<p><small>'+date+'</small></p>' +
+                            '</div>' +
+                        '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:100%;" src="'+you.avatar+'" /></div>' +
+                  '</li>';
+    }
+    setTimeout(
+        function(){
+            $("ul").append(control);
+
+        }, time);
+
+}
+
+function insertUserAction(user_action, time = 0){
+    console.log("Call insert user action");
+    console.log(user_action);
+    setTimeout(
+    function(){
+        $("#user_action").empty();
+        var content = '<p class="lead">' + JSON.stringify(user_action, null, 2) + '</p>'
+        $("#user_action").append(content);
+    }, time);
+}
+
+function submit(input_text) {
+    console.log("Call Submit");
+    console.log("Chatbot input:", input_text);
+	url = "/chatbot/goaloriented";
+	data = {"question": input_text};
+	$.ajax({
+	  type: "POST",
+	  url: url,
+	  contentType: 'application/json',
+	  data: JSON.stringify(data, null, '\t'),
+	  success: function(data) {
+	    // This is in JSON format, access different fields
+        var chatbot_reply = JSON.parse(data.chatbot_reply);
+        console.log(chatbot_reply);
+        console.log(chatbot_reply.system_action_nl);
+	  	insertChat("chatbot", chatbot_reply.system_action_nl, 1000);
+	  	insertUserAction(chatbot_reply.user_action, 1000);
+	  }
+	});
+}
+
+
+function resetChat(){
+    $("ul").empty();
+}
+
+$(".mytext").on("keyup", function(e){
+    if (e.which == 13){
+        var text = $(this).val();
+        if (text !== ""){
+            insertChat("me", text);
+            $(this).val('');
+            submit(text);
+        }
+    }
+});
+
+
+
+//-- NOTE: No use time on insertChat.
