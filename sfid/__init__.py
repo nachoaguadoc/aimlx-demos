b0337d175faea6f8067af778e360867fe3759d04
Fabian Germann <fabian.germann@swisscom.com>
2017-11-08 13:24:13 +0100

    put routes in separate controller file

--- sfid/__init__.py
+++ sfid/__init__.py
@@ -1,19 +1,5 @@
-from flask import Blueprint, jsonify, request, render_template
-import requests
-import config as conf
+from flask import Blueprint
 
 sfid = Blueprint('sfid', __name__, template_folder='templates', static_folder='static')
 
-
-@sfid.route('')
-def showSfidPage():
-    return render_template('sfid.html')
-
-
-@sfid.route('', methods=['POST'])
-def submitSfid():
-    parameters = request.get_json(force=True)
-    if request.method == 'POST':
-        result = requests.post(conf.sfid['url'], json=parameters)
-        resultdict = result.json()
-        return jsonify(resultdict)
+from . import sfid_controller
