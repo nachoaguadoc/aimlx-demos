d286d101c6a660d80b99841ac3619154acc515ec
Ignacio Aguado <nachoaguadoc@gmail.com>
2017-12-11 15:27:03 +0100

    Updated error page and server file

--- server.py
+++ server.py
@@ -22,9 +22,7 @@ import helpers
 
 from controller.chatbot_controller import chatbot_api
 from controller.churn_controller import churn_api
-#from controller.emotion_controller import emotion_api
 from controller.argumentation_controller import argumentation_api
-# from controller.grocery_controller import grocery_api
 from controller.kp_extraction_controller import kp_extraction_api
 from controller.machine_translation_controller import machine_translation_api
 from controller.gsw_controller import gsw_api
@@ -33,8 +31,8 @@ from controller.neural_programmer_controller import neural_programmer_api
 from controller.opinion_target_controller import opinion_target_api
 from controller.sfid_controller import sfid_api
 from controller.slot_filling_controller import slot_filling_api
-#from controller.summary_controller import summary_api
 from controller.seq2sql_controller import seq2sql_api
+
 from sfid import sfid
 from argumentation import argumentation
 from summarization import summarization
@@ -52,12 +50,12 @@ app.register_blueprint(opinion_target_api, url_prefix='/opinion')
 app.register_blueprint(churn_api, url_prefix='/churn')
 app.register_blueprint(ner_api, url_prefix='/ner')
 app.register_blueprint(kp_extraction_api, url_prefix='/kp')
-app.register_blueprint(summary_api, url_prefix='/summary')
 app.register_blueprint(machine_translation_api, url_prefix='/translate')
 app.register_blueprint(gsw_api, url_prefix='/gsw')
 app.register_blueprint(argumentation, url_prefix='/argumentation')
 app.register_blueprint(argumentation_api, url_prefix='/argumentation_old')
 app.register_blueprint(slot_filling_api, url_prefix='/slotfilling')
+
 app.register_blueprint(sfid, url_prefix='/sfid')
 app.register_blueprint(summarization, url_prefix='/summarization')
 app.register_blueprint(sfid_api, url_prefix='/sfid_old')
