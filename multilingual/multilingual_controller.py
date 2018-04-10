c3902fd35f2dbb91540a533f12f9211621d8c804
Meryem M'hamdi <meryemmhamdi1@gmail.com>
2018-04-10 16:53:13 +0200

    Added multilingual frontend

---
+++ multilingual/multilingual_controller.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+from flask import Flask, jsonify, request, render_template
+from . import multilingual
+app = Flask(__name__)
+
+
+@multilingual.route('/')
+def index():
+    return render_template('index.html')
+
+
+@multilingual.route('/alldocs', methods=['GET', 'POST'])
+def process_all_doc():
+    return render_template('multilingual_sample_docs.html')
+
+
+@multilingual.route('/sendalldocs', methods=['GET', 'POST'])
+def process_send_all_doc():
+    return render_template('multilingual_sample_docs.html')
+
+
+@multilingual.route('/choosedoc', methods=['GET', 'POST'])
+def process_choose_doc():
+    return render_template('multilingual_choose_doc.html')
