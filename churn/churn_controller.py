0e010bb633c3c6cee5a20523e9f952732ddc974b
abbet <abbet.christian@gmail.com>
2018-04-24 13:26:44 +0200

    Merge remote-tracking branch 'upstream/develop'
    Conflicts:
    	templates/error.html
    	templates/index.html
    
    Add feed back for bot
    Add <p> tag
    -

--- churn/churn_controller.py
+++ churn/churn_controller.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request, render_template, session
+from flask import request, render_template, session
 import requests
 import config as conf
 from . import churn
@@ -17,14 +17,13 @@ def submitChrun():
     if request.method == 'POST':
 
         text = parameters['input'].lower()
+        label_answer = None
 
         # 1. Check if still labels to do = answer is related to churn/non churn
         if 'churn' in session and len(session['churn']) != 0:
             # Compose msg to update db
-            send_label(session['text'], session['lang'], session['churn'][-1][0], session['churn'][-1][1], text)
-            print(session['churn'])
+            label_answer = send_label(session['text'], session['lang'], session['churn'][-1][0], session['churn'][-1][1], text)
             session['churn'] = session['churn'][:-1]
-            print(session['churn'])
         else:
             result = send_request(text)
             if result is None:
@@ -38,10 +37,15 @@ def submitChrun():
         if len(session['churn']) != 0:
             # 2.a. There are still brands to label -> send result for new tweet
             data_msg = {'lang': session['lang'], 'brand': session['churn'][-1][0], 'label': session['churn'][-1][1]}
-            return json.dumps(data_msg)
         else:
             # 2.b. It was the last one. Wat for new entry
-            return json.dumps({'msg': 1, 'lang': session['lang']})
+            data_msg = {'msg': 1, 'lang': session['lang']}
+
+        # 3. Add answer to previous label if available
+        if label_answer is not None:
+            data_msg['label_answer'] = label_answer
+
+        return json.dumps(data_msg)
 
 
 def send_request(text):
@@ -58,7 +62,13 @@ def send_request(text):
 def send_label(text, lang, brand, label, cgt):
     try:
         data = {'text': text, 'lang': lang, 'brand': brand, 'label': label, 'cgt': cgt}
-        requests.post(conf.churn['api_url'] + 'churn_label', data=json.dumps(data))
+        result = requests.post(conf.churn['api_url'] + 'churn_label', data=json.dumps(data))
+        result = json.loads(result.text)
+        if result is not None and 'correct' in result:
+            print('Answer was :', result['correct'])
+            return result['correct']
+        else:
+            return None
     except Exception as e:
         print('Error:', e)
-    return
+    return None
