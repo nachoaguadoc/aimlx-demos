c781e012bfe42344e5909b5de684e7b90bcb9b74
Gramet <gaetan.ramet@gmail.com>
2018-04-25 16:41:34 +0200

    forgot to add the new route to the controller
    Signed-off-by: Gramet <gaetan.ramet@gmail.com>

--- speech_emotion/speech_emotion_controller.py
+++ speech_emotion/speech_emotion_controller.py
@@ -22,6 +22,18 @@ def identify():
         response = Response(resp.content, resp.status_code, headers)
         return response
 
+@speech_emotion.route('/record', methods=['POST'])
+def identify():
+    if request.method == 'POST':
+        record_url = os.path.join(conf.speech_emotion['url'], "record")
+        resp = requests.request(method=request.method, url=record_url, headers={key: value for (key, value) in request.headers if key != 'Host'}, data=request.get_data(), cookies=request.cookies, allow_redirects=False)
+
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
+
+        response = Response(resp.content, resp.status_code, headers)
+        return response
+
 @speech_emotion.route('/<path:path>', methods=['GET'])
 def send_js(path):
     print("PATH:")
