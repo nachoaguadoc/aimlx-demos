892f6cf294673f1ab703e86bb619781ce64cb767
Es-Sami Karim, ENT-DES-SEA-PDP <Karim.Es-Sami@swisscom.com>
2018-06-07 14:13:23 +0200

    Send the demos from the backend to the frontend

--- controller/landingpage_controller.py
+++ controller/landingpage_controller.py
@@ -1,12 +1,20 @@
-from flask import Blueprint
+import json
+
+from flask import Blueprint, jsonify
 from flask import send_from_directory
-from werkzeug.utils import redirect
 
 PATH_NESTED_LANDING_PAGE = 'landingpage/'
+
 landingpage_api = Blueprint('landingpage_api', __name__)
 
 
-# Chatbot route handling
-@landingpage_api.route('/')
+@landingpage_api.route('')
 def get_landingpage():
     return send_from_directory(PATH_NESTED_LANDING_PAGE, 'index.html')
+
+
+@landingpage_api.route('demos')
+def get_all_demos_json():
+    with open('demos.json') as f:
+        demos_dict = json.load(f)
+        return jsonify(demos_dict)
